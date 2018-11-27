a1558ed16cb2a3efceca933685ba9c77c69c0b05
gasdulis <benjamin.tiennot@hei.yncrea.fr>
2018-11-27 12:08:59 +0100

    files commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC2Dre-K6YKHx6LkdTPfK-ntzAQYcS6brw",
+    authDomain: "totapp-isa.firebaseapp.com",
+    databaseURL: "https://totapp-isa.firebaseio.com",
+    projectId: "totapp-isa",
+    storageBucket: "totapp-isa.appspot.com",
+    messagingSenderId: "953346730350"
+  }
+
+
+};
