273c2c3a048f92877cdc087ecd36ceeed90201a0
Pau Alos Mairal <paualos3@gmail.com>
2018-11-27 17:47:09 +0100

    test of production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2Dre-K6YKHx6LkdTPfK-ntzAQYcS6brw",
+    authDomain: "totapp-isa.firebaseapp.com",
+    databaseURL: "https://totapp-isa.firebaseio.com",
+    projectId: "totapp-isa",
+    storageBucket: "totapp-isa.appspot.com",
+    messagingSenderId: "953346730350"
+  }
 };
